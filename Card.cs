81f4ba65f3a337fab29cfe2949ec3b9f3c6d8df5
Eric Xue <eric.xy.xue@gmail.com>
2020-10-22 17:48:31 -0700

    Remove flag values

--- Card.cs
+++ Card.cs
@@ -2,18 +2,18 @@
 {
   public enum Card
   {
-    ExplodingKitten     = 1,
-    Defuse              = 1 << 1,
-    Nope                = 1 << 2,
-    Attack2x            = 1 << 3,
-    Skip                = 1 << 4,
-    Favor               = 1 << 5,
-    Shuffle             = 1 << 6,
-    SeeTheFuture3x      = 1 << 7,
-    Tacocat             = 1 << 8,
-    Cattermelon         = 1 << 9,
-    HairyPotatoCat      = 1 << 10,
-    BeardCat            = 1 << 11,
-    RainbowRalphingCat  = 1 << 12,
+    ExplodingKitten,
+    Defuse,
+    Nope,
+    Attack2X,
+    Skip,
+    Favor,
+    Shuffle,
+    SeeTheFuture3X,
+    Tacocat,
+    Cattermelon,
+    HairyPotatoCat,
+    BeardCat,
+    RainbowRalphingCat,
   }
 }
