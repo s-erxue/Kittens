c96179adaa46a7ad1ae9ef41af7a197242ca9ef6
Eric Xue <eric.xy.xue@gmail.com>
2020-10-22 18:02:28 -0700

    Fix errors

--- Sets/Original.cs
+++ Sets/Original.cs
@@ -17,7 +17,7 @@ namespace Kittens.Sets
       int nextPlayerTurns;
       while (currentPlayer.TurnsLeft > 0)
       {
-        nextPlayerTurns = PlayOrPass(currentPlayer, deck, players, currentPlayer.turnsLeft);
+        nextPlayerTurns = PlayOrPass(currentPlayer, deck, players, currentPlayer.TurnsLeft);
       }
     }
 
@@ -34,95 +34,108 @@ namespace Kittens.Sets
 3. Pass
 
 > ", 3))
-        {
-          case 1:
-            Console.WriteLine("Your hand:\n");
-            foreach (Card card in currentPlayer.Hand)
-            {
-              PrintCardWithNewline(card);
-            }
-            break;
-          case 2:
-            switch (GetIntFromRange(@"How to play cards?
+          {
+            case 1:
+              Console.WriteLine("Your hand:\n");
+              foreach (Card card in currentPlayer.Hand)
+              {
+                PrintCardWithNewline(card);
+              }
+
+              break;
+            case 2:
+              switch (GetIntFromRange(@"How to play cards?
 
 1. Play single card
 2. Play special combo
 
 > ", 2))
-            {
-              case 1:
-                List<Card> actionCards = currentPlayer.Hand.Where(
-                  c =>
-                    Array.Exists(
-                      new[] { Card.Attack2X, Card.Skip, Card.Favor, Card.Shuffle, Card.SeeTheFuture3X },
-                      card => card == c
+              {
+                case 1:
+                  List<Card> actionCards = currentPlayer.Hand.Where(
+                      c =>
+                        Array.Exists(
+                          new[] {Card.Attack2X, Card.Skip, Card.Favor, Card.Shuffle, Card.SeeTheFuture3X},
+                          card => card == c
+                        )
                     )
-                )
-                  .Distinct()
-                  .ToList();
-                Console.WriteLine("Which one?\n");
-                for (int i = 0; i < actionCards.Count(); i++)
-                {
-                  Console.Write($"{i + 1}. ");
-                  PrintCardWithNewline(actionCards[i]);
-                }
-                Card cardToPlay = actionCards[GetIntFromRange("\n> ", actionCards.Count) - 1];
-                // ReSharper disable SwitchStatementMissingSomeEnumCasesNoDefault
-                switch (cardToPlay)
-                // ReSharper restore SwitchStatementMissingSomeEnumCasesNoDefault
-                {
+                    .Distinct()
+                    .ToList();
+                  Console.WriteLine("Which one?\n");
+                  for (int i = 0; i < actionCards.Count(); i++)
+                  {
+                    Console.Write($"{i + 1}. ");
+                    PrintCardWithNewline(actionCards[i]);
+                  }
+
+                  Card cardToPlay = actionCards[GetIntFromRange("\n> ", actionCards.Count) - 1];
+                  // ReSharper disable SwitchStatementMissingSomeEnumCasesNoDefault
+                  switch (cardToPlay)
+                    // ReSharper restore SwitchStatementMissingSomeEnumCasesNoDefault
+                  {
                     case Card.Attack2X:
-                        break;
+                      break;
                     case Card.Skip:
                       turnsLeftLocal--;
                       break;
                     case Card.Favor:
-                        List<Player> playersToStealFrom = players
-                            .Where(player => player != currentPlayer && player.Hand.Count != 0)
-                            .ToList();
-                        Console.WriteLine("Who to steal from?\n");
-                        for (int i = 0; i < playersToStealFrom.Count; i++)
-                        {
-                            Console.WriteLine($"{i + 1}. {playersToStealFrom[i].Name}");
-                        }
-                        Player playerToStealFrom = playersToStealFrom[GetIntFromRange("\n>", 1, playersToStealFrom.Count) - 1];
-                        Console.Write($"{playerToStealFrom.Name}, press a key when you are ready.");
-                        Console.ReadKey();
-                        Console.WriteLine("Which card to give?\n");
-                        for (int i = 0; i < playerToStealFrom.Hand.Count; i++)
-                        {
-                            Console.Write($"{i + 1}. ");
-                            PrintCardWithNewline(playerToStealFrom.Hand[i]);
-                        }
-                        int cardToStealIndex = GetIntFromRange("\n>", 1, playerToStealFrom.Hand.Count) - 1;
-                        currentPlayer.Hand.Add(playerToStealFrom.Hand[cardToStealIndex]);
-                        playerToStealFrom.Hand.RemoveAt(cardToStealIndex);
-                        break;
+                      List<Player> playersToStealFrom = players
+                        .Where(player => player != currentPlayer && player.Hand.Count != 0)
+                        .ToList();
+                      Console.WriteLine("Who to steal from?\n");
+                      for (int i = 0; i < playersToStealFrom.Count; i++)
+                      {
+                        Console.WriteLine($"{i + 1}. {playersToStealFrom[i].Name}");
+                      }
+
+                      Player playerToStealFrom =
+                        playersToStealFrom[GetIntFromRange("\n>", 1, playersToStealFrom.Count) - 1];
+                      Console.Write($"{playerToStealFrom.Name}, press a key when you are ready.");
+                      Console.ReadKey();
+                      Console.WriteLine("Which card to give?\n");
+                      for (int i = 0; i < playerToStealFrom.Hand.Count; i++)
+                      {
+                        Console.Write($"{i + 1}. ");
+                        PrintCardWithNewline(playerToStealFrom.Hand[i]);
+                      }
+
+                      int cardToStealIndex = GetIntFromRange("\n>", 1, playerToStealFrom.Hand.Count) - 1;
+                      currentPlayer.Hand.Add(playerToStealFrom.Hand[cardToStealIndex]);
+                      playerToStealFrom.Hand.RemoveAt(cardToStealIndex);
+                      break;
                     case Card.Shuffle:
-                        deck.Shuffle();
-                        break;
+                      deck.Shuffle();
+                      break;
                     case Card.SeeTheFuture3X:
-                        Console.WriteLine("Cards from top to bottom:\n");
-                        List<Card> topThree = deck.GetRange(deck.Count - 3, 3);
-                        topThree.Reverse();
-                        foreach (Card card in topThree)
-                        {
-                            PrintCardWithNewline(card);
-                        }
-                        break;
-                }
-                break;
-              case 2:
-                break;
-            }
-            break;
-          case 3:
-            return 1;
+                      Console.WriteLine("Cards from top to bottom:\n");
+                      List<Card> topThree = deck.GetRange(deck.Count - 3, 3);
+                      topThree.Reverse();
+                      foreach (Card card in topThree)
+                      {
+                        PrintCardWithNewline(card);
+                      }
+
+                      break;
+                  }
+
+                  break;
+                case 2:
+                  break;
+              }
+
+              break;
+            case 3:
+              return 1;
+          }
         }
       }
 
       return 1;
     }
+    
+    
+      
+      
 
     private static (List<Player> players, int numPlayers, List<Card> deck) Setup()
     {
